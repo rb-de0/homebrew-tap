a75a2c7e42c2c0f91df12340edeedf5875b93c3c
rb_de0 <rebirth.de0@gmail.com>
2019-02-24 02:05:46 +0900

    Fix checksum

--- linuxmain-gen.rb
+++ linuxmain-gen.rb
@@ -4,7 +4,7 @@ class LinuxmainGen < Formula
     homepage "https://github.com/rb-de0/LinuxMainGen"
     version "0.2.0"
     url "https://github.com/rb-de0/LinuxMainGen/releases/download/#{version}/linuxmain-gen"
-    sha256 "d059649a25e172af0937e8404b0592ecd14afb1b8acd4f6db3e6836a94d2e73b"
+    sha256 "ab75d076bf9b4b9bbd5e2673d0b294b52f286211e9f29ee6b9c078b2707730ea"
   
     def install
       bin.install "linuxmain-gen"
