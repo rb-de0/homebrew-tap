4afadf233477aeaf3240dc1c46b0162b2c602301
rb_de0 <rebirth.de0@gmail.com>
2019-02-24 02:03:10 +0900

    Update linuxmain-gen

--- linuxmain-gen.rb
+++ linuxmain-gen.rb
@@ -2,7 +2,7 @@
 class LinuxmainGen < Formula
     desc "A tool to automatically generate LinuxMain.swift"
     homepage "https://github.com/rb-de0/LinuxMainGen"
-    version "0.1.0"
+    version "0.2.0"
     url "https://github.com/rb-de0/LinuxMainGen/releases/download/#{version}/linuxmain-gen"
     sha256 "d059649a25e172af0937e8404b0592ecd14afb1b8acd4f6db3e6836a94d2e73b"
   
